70ea8cb5fb1b37437c125a2b9a58e239c5ddf8a8
Evgeny <evga.1998@mail.ru>
2018-03-20 18:38:55 +0300

    initial commit

---
+++ Task1/FileSystemVisitor.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.IO;
+using System.Collections;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+
+namespace Task1
+{
+    public class FileSystemVisitor : IEnumerable
+    {
+        private StringCollection _files = new StringCollection();
+
+        private readonly Func<string, bool> _filter;
+
+        public FileSystemVisitor(string path, Func<string, bool> filter = null)
+        {
+            _filter = filter;
+
+            foreach (var file in GetDirectoryFiles(path))
+            {
+                _files.Add(file);
+            }
+        }
+
+        private IEnumerable<string> GetDirectoryFiles(string path)
+        {
+            DirectoryInfo directory = new DirectoryInfo(path);
+
+            foreach (var file in directory.GetFiles())
+            {               
+                if (_filter == null || _filter(file.FullName)) yield return file.FullName;
+            }
+
+            foreach (DirectoryInfo dirInfo in directory.GetDirectories())
+            {
+                if (_filter == null || _filter(dirInfo.FullName)) yield return dirInfo.FullName;
+
+                foreach (var file in GetDirectoryFiles(dirInfo.FullName))
+                {
+                    if (_filter == null || _filter(file)) yield return file;
+                }
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+        private FileSystemVisitorEnumerator GetEnumerator() => new FileSystemVisitorEnumerator(this);
+
+        private struct FileSystemVisitorEnumerator : IEnumerator
+        {
+            private readonly FileSystemVisitor fileSystemVisitor;
+            private int index;
+
+            public FileSystemVisitorEnumerator(FileSystemVisitor visitor) : this()
+            {
+                fileSystemVisitor = visitor;
+                index = -1;
+            }
+
+            public bool MoveNext() => ++index < fileSystemVisitor._files.Count;
+
+            public string Current
+            {
+                get
+                {
+                    if (index <= -1 || index >= fileSystemVisitor._files.Count)
+                        throw new InvalidOperationException();
+                    return fileSystemVisitor._files[index];
+                }
+            }
+
+            object IEnumerator.Current => Current;
+
+            void IEnumerator.Reset()
+            {
+                index = -1;
+            }
+        }
+    }
+}
---
+++ Task1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Specialized;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string rootDir = @"C:\Users\Evgeny_Ermolovich\Desktop\Training\CreateDB";
+            FileSystemVisitor a = new FileSystemVisitor(rootDir);
+            
+            foreach (var item in a)
+            {
+                Console.WriteLine(item);
+            }
+
+            Console.WriteLine("Press any key");
+            Console.ReadKey();
+        }
+    }
+}
