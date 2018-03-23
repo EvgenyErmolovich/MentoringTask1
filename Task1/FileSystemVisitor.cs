ea05b19833b563c6f962b1ac34fb4dfbe4f07e42
Evgeny <evga.1998@mail.ru>
2018-03-23 09:56:30 +0300

    XML coments added

--- Task1/FileSystemVisitor.cs
+++ Task1/FileSystemVisitor.cs
@@ -2,30 +2,67 @@
 using System.IO;
 using System.Collections;
 using System.Collections.Generic;
-using System.Collections.Specialized;
 
 namespace Task1
 {
     public class FileSystemVisitor : IEnumerable
     {
+        /// <summary>
+        /// List of finded files and foldes
+        /// </summary>
         private List<FileSystemInfo> _files;
+        /// <summary>
+        /// Path of directory in which start finding files and folders
+        /// </summary>
         private readonly string _path;
+        /// <summary>
+        /// Delegate for filtering files and folders
+        /// </summary>
         private readonly Func<FileSystemInfo, bool> _filter;
         private bool _stopSearch;
 
+        /// <summary>
+        /// Flag to stop searching files and folders
+        /// </summary>
         public bool StopSearchFlag { get; set; }
+        /// <summary>
+        /// Flag to delete file or folder
+        /// </summary>
         public bool DeleteElementFlag { get; set; }
 
         public delegate void EventDelegate();
         public delegate void ElementFindedDelegate(string elementFullName);
 
+        /// <summary>
+        /// Event for starting search
+        /// </summary>
         public event EventDelegate Start;
+        /// <summary>
+        /// Event for finishing search
+        /// </summary>
         public event EventDelegate Finish;
+        /// <summary>
+        /// Event for finded file
+        /// </summary>
         public event ElementFindedDelegate FileFinded;
+        /// <summary>
+        /// Event for finded folder
+        /// </summary>
         public event ElementFindedDelegate DirectoryFinded;
+        /// <summary>
+        /// Event for filtered finded file
+        /// </summary>
         public event ElementFindedDelegate FilteredFileFinded;
+        /// <summary>
+        /// Event for filtered finded folder
+        /// </summary>
         public event ElementFindedDelegate FilteredDirectoryFinded;
 
+        /// <summary>
+        /// Constructor for creating object FileSystemVisitor
+        /// </summary>
+        /// <param name="path">Path of directory in which start finding files and folders</param>
+        /// <param name="filter">Delegate for filtering files and folders</param>
         public FileSystemVisitor(string path, Func<FileSystemInfo, bool> filter = null)
         {
             _files = new List<FileSystemInfo>();
@@ -33,6 +70,9 @@ namespace Task1
             _filter = filter;
         }
 
+        /// <summary>
+        /// Start finding files and folders
+        /// </summary>
         public void Execute()
         {
             Start?.Invoke();
@@ -46,14 +86,14 @@ namespace Task1
 
                     if (!DeleteElementFlag) _files.Add(file);
                     DeleteElementFlag = false;
-                    _stopSearch = true;
+                    _stopSearch = StopSearchFlag;
                 }
                 else
                 {
                     if (file.GetType() == typeof(FileInfo)) FilteredFileFinded?.Invoke(file.FullName);
                     else FilteredDirectoryFinded?.Invoke(file.FullName);
 
-                    if (!DeleteElementFlag) _files.Add(file);
+                    if (!DeleteElementFlag && _filter(file)) _files.Add(file);
                     DeleteElementFlag = false;
                     _stopSearch = StopSearchFlag;
                 }
@@ -61,6 +101,11 @@ namespace Task1
             Finish?.Invoke();
         }
 
+        /// <summary>
+        /// Finding files and folsers in folder
+        /// </summary>
+        /// <param name="path">Folder in which find files and folders</param>
+        /// <returns>FileInfo and DirectoryInfo which were finded in current folder</returns>
         private IEnumerable<FileSystemInfo> GetDirectoryFiles(string path)
         {
             DirectoryInfo directory = new DirectoryInfo(path);
