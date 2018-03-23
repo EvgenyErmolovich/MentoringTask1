daf541cac778fc02fe99061cea3d244b6e0289b5
Evgeny <evga.1998@mail.ru>
2018-03-23 09:36:07 +0300

    Refactor, StringCollection changed to List<FileSystemInfo>

--- Task1/FileSystemVisitor.cs
+++ Task1/FileSystemVisitor.cs
@@ -8,7 +8,7 @@ namespace Task1
 {
     public class FileSystemVisitor : IEnumerable
     {
-        private StringCollection _files;
+        private List<FileSystemInfo> _files;
         private readonly string _path;
         private readonly Func<FileSystemInfo, bool> _filter;
         private bool _stopSearch;
@@ -28,7 +28,7 @@ namespace Task1
 
         public FileSystemVisitor(string path, Func<FileSystemInfo, bool> filter = null)
         {
-            _files = new StringCollection();
+            _files = new List<FileSystemInfo>();
             _path = path;
             _filter = filter;
         }
@@ -38,54 +38,45 @@ namespace Task1
             Start?.Invoke();
             foreach (var file in GetDirectoryFiles(_path))
             {
-                _files.Add(file);
                 if (_stopSearch) break;
+                if (_filter == null)
+                {
+                    if (file.GetType() == typeof(FileInfo)) FileFinded?.Invoke(file.FullName);
+                    else DirectoryFinded?.Invoke(file.FullName);
+
+                    if (!DeleteElementFlag) _files.Add(file);
+                    DeleteElementFlag = false;
+                    _stopSearch = true;
+                }
+                else
+                {
+                    if (file.GetType() == typeof(FileInfo)) FilteredFileFinded?.Invoke(file.FullName);
+                    else FilteredDirectoryFinded?.Invoke(file.FullName);
+
+                    if (!DeleteElementFlag) _files.Add(file);
+                    DeleteElementFlag = false;
+                    _stopSearch = StopSearchFlag;
+                }
             }
             Finish?.Invoke();
         }
 
-        private IEnumerable<string> GetDirectoryFiles(string path)
+        private IEnumerable<FileSystemInfo> GetDirectoryFiles(string path)
         {
             DirectoryInfo directory = new DirectoryInfo(path);
 
             foreach (var file in directory.GetFiles())
             {
-                if (_filter == null)
-                {
-                    FileFinded?.Invoke(file.FullName);
-                    if(!DeleteElementFlag) yield return file.FullName;
-                    DeleteElementFlag = false;
-                    _stopSearch = StopSearchFlag;
-                }
-                else
-                {
-                    FilteredFileFinded?.Invoke(file.FullName);
-                    if (_filter(file) && !DeleteElementFlag) yield return file.FullName;
-                    DeleteElementFlag = false;
-                    _stopSearch = StopSearchFlag;
-                }
+                yield return file;
             }
 
             foreach (DirectoryInfo dirInfo in directory.GetDirectories())
             {
-                if (_filter == null)
-                {
-                    DirectoryFinded?.Invoke(dirInfo.FullName);
-                    if(!DeleteElementFlag) yield return dirInfo.FullName;
-                    DeleteElementFlag = false;
-                    _stopSearch = StopSearchFlag;
-                }
-                else
-                {
-                    FilteredDirectoryFinded?.Invoke(dirInfo.FullName);
-                    if (_filter(dirInfo) && !DeleteElementFlag) yield return dirInfo.FullName;
-                    DeleteElementFlag = false;
-                    _stopSearch = StopSearchFlag;
-                }
+                yield return dirInfo;
 
                 foreach (var file in GetDirectoryFiles(dirInfo.FullName))
                 {
-                     yield return file;
+                    yield return file;
                 }
             }
         }
@@ -98,7 +89,7 @@ namespace Task1
             private readonly FileSystemVisitor fileSystemVisitor;
             private int index;
 
-            public FileSystemVisitorEnumerator(FileSystemVisitor visitor) 
+            public FileSystemVisitorEnumerator(FileSystemVisitor visitor)
             {
                 fileSystemVisitor = visitor;
                 index = -1;
@@ -106,7 +97,7 @@ namespace Task1
 
             public bool MoveNext() => ++index < fileSystemVisitor._files.Count;
 
-            public string Current
+            public FileSystemInfo Current
             {
                 get
                 {
